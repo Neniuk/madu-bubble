616bf9425f4ee7916a338b342f182848dc011ef6
DucLUT <minhduc16032004@gmail.com>
2025-01-24 20:18:57 +0200

    adding movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -17,21 +17,27 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
+	var direction_x := Input.get_axis("move_left", "move_right")
+	var direction_y := Input.get_axis("move_up", "move_down")
 	
-	if direction > 0:
+	if direction_x > 0:
 		animated_sprite_2d.flip_h = false
-	elif direction < 0:
+	elif direction_x < 0:
 		animated_sprite_2d.flip_h = true
 	
-	if direction == 0:
+	if direction_x == 0 && direction_y == 0:
 		animated_sprite_2d.play("idle")
 	else:
 		animated_sprite_2d.play("walk")
 	
-	if direction:
-		velocity.x = direction * SPEED
+	if direction_x:
+		velocity.x = direction_x * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if direction_y:
+		velocity.y = direction_y * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
