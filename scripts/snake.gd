622c8cc23a46822f9afec9f25f50cea985ee423d
DucLUT <minhduc16032004@gmail.com>
2025-01-25 19:23:49 +0200

    adding change direction for snake

--- scripts/snake.gd
+++ scripts/snake.gd
@@ -3,6 +3,7 @@ extends Node2D
 const SPEED = 60
 @onready var ray_cast_up: RayCast2D = $RayCastUp
 @onready var ray_cast_down: RayCast2D = $RayCastDown
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 var direction = 1
 
@@ -15,6 +16,8 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if ray_cast_down.is_colliding():
 		direction = -1
+		animated_sprite_2d.flip_v = true
 	if ray_cast_up.is_colliding():
 		direction = 1
+		animated_sprite_2d.flip_v = false
 	position.y += direction * SPEED * delta
